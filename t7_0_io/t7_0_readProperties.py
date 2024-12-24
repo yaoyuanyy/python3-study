e4f7107cdd970d759b50932d93947f5b06660d41
yaoyihao1 <yaoyihao1@gmail.com>
2024-12-24 08:19:53 +0800

    oop

--- t7_0_io/t7_0_readProperties.py
+++ t7_0_io/t7_0_readProperties.py
@@ -5,3 +5,22 @@ with open(fpath, "r", encoding="utf-8") as f:
     for readline in f.readlines():
         # 把末尾的"\n" 删掉
         print(readline.strip())
+
+
+# 写入文件
+with open(fpath, "t+a") as f:
+    f.writelines("\nheight=10")
+
+
+readFpath = "/Users/skyler/Documents/projects-pycharm/python3-study/t7_0_io/python_img_1.png"
+writeFpath = "/Users/skyler/Documents/projects-pycharm/python3-study/t7_0_io/python_img_1.bak.png"
+
+# 读取一个图片，写入另一个文件+bak
+
+with open(writeFpath, 'wb') as writeF:
+    with open(readFpath, "rb") as readF:
+        bData = readF.read()
+        print("len", bData)
+        writeF.write(bData)
+
+
